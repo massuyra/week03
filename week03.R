ba2d83ac544f84f366f961bd9e7dad7b32ca9a6b
massuyra <a.massuyr@gmail.com>
2023-10-20 23:22:57 +0100

    deaths mean

--- week03.R
+++ week03.R
@@ -1,6 +1,25 @@
 library(tidyverse)
 emdat <- read.csv("EMDAT.csv")
 glimpse(emdat)
+str(emdat)
 view(emdat)
-emdat %>%
-  select(contains("deaths"||"injured"||"homeless"))
+emdat_renamed <- emdat %>%
+  rename(
+    entity = Entity, 
+    year = Year)
+affected <- emdat_renamed %>%
+  select("entity", "year", "deaths_all_disasters", "injured_all_disasters", "homeless_all_disasters")
+view(affected)
+str(affected)
+deaths_total_per_country <- affected %>%
+  select("entity", "year", "deaths_all_disasters") %>%
+  pivot_wider(
+    names_from = entity,
+    values_from = deaths_all_disasters
+  )
+deaths_mean_per_country <- deaths_total_per_country %>%
+  colMeans(na.rm=TRUE) %>%
+  round(2)
+deaths_mean_per_country_df <- as.data.frame(deaths_mean_per_country) %>%
+  arrange(desc(deaths_mean_per_country))
+deaths_mean_per_country_df
