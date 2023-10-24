2e55ab0f423a975ea7a3919c3209a571905a1357
massuyra <a.massuyr@gmail.com>
2023-10-24 23:54:55 +0100

    corrected

--- week03.R
+++ week03.R
@@ -1,28 +1,57 @@
-rm(list = ls())
-library(tidyverse)
+pacman::p_load(       
+  tidyverse,
+  janitor,
+  skimr,
+  kableExtra,
+  flextable
+)
 emdat <- read.csv("EMDAT.csv")
 glimpse(emdat)
 str(emdat)
+skim(emdat)
 view(emdat)
-emdat_renamed <- emdat %>%
-  rename(
-    entity = Entity, 
-    year = Year)
-affected <- emdat_renamed %>%
+affected <- clean_names(emdat) %>%
   select("entity", "year", "deaths_all_disasters", "injured_all_disasters", "homeless_all_disasters")
 view(affected)
-str(affected)
-deaths_total_per_country <- affected %>%
-  select("entity", "year", "deaths_all_disasters") %>%
+avg_dih <- affected %>%
+  group_by(entity) %>%
+  summarize(
+    avg_deaths = mean(deaths_all_disasters, na.rm = TRUE) %>% round(2),
+    avg_injured = mean(injured_all_disasters, na.rm = TRUE) %>% round(2),
+    avg_homeless = mean(homeless_all_disasters, na.rm = TRUE) %>% round(2)
+  )
+view(avg_dih)
+avg_deaths_ranked = avg_dih %>%
+  select(entity, avg_deaths) %>%
+  arrange(desc(avg_deaths)) %>%
+  slice(1:11)
+avg_deaths_ranked_table <- kable(avg_deaths_ranked) %>%
+  kable_styling()
+avg_injured_ranked_table <- kable(
+  avg_dih %>%
+    select(entity, avg_injured) %>%
+    arrange(desc(avg_injured)) %>%
+    slice(1:11)
+) %>%
+  kable_styling()
+avg_injured_ranked_table
+avg_homeless_ranked_table <- kable(
+  avg_dih %>%
+    select(entity, avg_homeless) %>%
+    arrange(desc(avg_homeless)) %>%
+    slice(1:11)
+) %>%
+  kable_styling()
+avg_homeless_ranked_table
+emdat_deadly <- clean_names(emdat) %>%
+  select(entity, year, deaths_all_disasters) %>%
+  mutate(
+    deadly = ifelse(deaths_all_disasters > 500, 1, 0)
+  )
+view(emdat_deadly)
+affected_wider <- affected %>%
   pivot_wider(
     names_from = entity,
-    values_from = deaths_all_disasters
+    values_from = c(deaths_all_disasters, injured_all_disasters, homeless_all_disasters)
   )
-view(deaths_total_per_country)
-deaths_mean_per_country <- deaths_total_per_country %>%
-  colMeans(na.rm=TRUE) %>%
-  round(2)
-deaths_mean_per_country_df <- as.data.frame(deaths_mean_per_country) %>%
-  arrange(desc(deaths_mean_per_country))
-deaths_mean_per_country_df
-
+view(affected_wider)
